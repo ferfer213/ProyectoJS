a9509e72a4fc583bd98cbb9c19a94faebf2a364b
Fernando Salcedo <fsaprogramacion@gmail.com>
2022-09-15 20:06:37 -0300

    casi final

--- script.js
+++ script.js
@@ -88,8 +88,6 @@ function mostrarCatalogo(params){
     
 //const del CARRITO 
 
-
-
 const fcompra = document.getElementById(`botonFinalizarCompra`)
 
 const vaciarCarrito = document.getElementById(`vaciarCarrito`)
@@ -99,6 +97,60 @@ const contenedorCarrito = document.getElementById(`modal-body`)
 const precioTotal = document.getElementById (`precioTotal`)
 
 const contadorCarrito = document.getElementById(`contadorCarrito`)
+
+
+const agregarAlCarrito = (prodId) => {
+    const existe = carrito.some (prod => prod.id === prodId)
+
+    if (existe){
+       const prod = carrito.map(prod => {
+            prod.id === prodId &&
+                prod.cantidad++
+            }
+        )
+    }
+
+    else{
+    const item = stockProductos.find ((prod) => prod.id === prodId)
+    const prod = carrito.map(prod => {
+        prod.id =! prodId &&
+            prod.cantidad + 1})
+    carrito.push(item)
+    
+    }
+    actualizarCarrito()
+    
+  }
+
+const actualizarCarrito = ()=>{
+    
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach((productoCarrito) =>{
+        const div = document.createElement(`div`)
+        div.className = (`modal-body`)
+        div.innerHTML = `
+        <div class="card border-primary mb-3" id ="productoCarrito${productoCarrito.id}" style="max-width: 540px;">
+                <img class="card-img-top" src="${productoCarrito.img}" alt="${productoCarrito.nombre}">
+                <div class="card-body">
+                        <h4 class="card-title">${productoCarrito.nombre}</h4>
+                    
+                        <p class="card-text">$${productoCarrito.precio}</p>
+                        <p>Cantidad:${productoCarrito.cantidad}</p>
+                        <button onclick="sumarAlCarrito"  class= "btn btn-success" id="botonAgregar"><i class="fas fa-plus"></i></button><button onclick="eliminarDelCarrito(${productoCarrito.id})" class= "btn btn-danger" id="botonRestar"><i class="fas fa-minus"></i></i></button>
+                        <button onclick="eliminarDelCarrito(${productoCarrito.id})" class= "btn btn-dark" id="botonEliminar"><i class="fas fa-trash-alt"></i></button>
+                </div>    
+            
+            
+            </div>`
+        
+        contenedorCarrito.appendChild(div)
+
+        localStorage.setItem(`carrito`, JSON.stringify(carrito))
+
+        console.log(productoCarrito.cantidad)
+    })
+
 
 document.addEventListener(`DOMContentLoaded`,()=>{
     if(localStorage.getItem(`carrito`)){
@@ -117,64 +169,22 @@ document.addEventListener(`DOMContentLoaded`,()=>{
 
  //CARRITO   
 
-  const agregarAlCarrito = (prodId) => {
-    const existe = carrito.some (prod => prod.id === prodId)
-
-    if (existe){
-        carrito.map(prod => {
-            prod.id === prodId &&
-                prod.cantidad++
-            }
-        )
-    }
-
-    else{
-    const item = stockProductos.find ((prod) => prod.id === prodId)
-    carrito.push(item)
-    }
-    actualizarCarrito()
-    
-  }
+  
 
 
 
 
 
 
-  const actualizarCarrito = ()=>{
-    
-    contenedorCarrito.innerHTML = ""
-
-    carrito.forEach((productoCarrito) =>{
-        const div = document.createElement(`div`)
-        div.className = (`modal-body`)
-        div.innerHTML = `
-        <div class="card border-primary mb-3" id ="productoCarrito${productoCarrito.id}" style="max-width: 540px;">
-                <img class="card-img-top" src="${productoCarrito.img}" alt="${productoCarrito.nombre}">
-                <div class="card-body">
-                        <h4 class="card-title">${productoCarrito.nombre}</h4>
-                    
-                        <p class="card-text">$${productoCarrito.precio}</p>
-                        <p>Cantidad:${productoCarrito.cantidad}</p>
-                        <button onclick="sumarAlCarrito(${productoCarrito.cantidad})" class= "btn btn-success" id="botonAgregar"><i class="fas fa-plus"></i></button><button onclick="eliminarDelCarrito(${productoCarrito.id})" class= "btn btn-danger" id="botonRestar"><i class="fas fa-minus"></i></i></button>
-                        <button onclick="eliminarDelCarrito(${productoCarrito.id})" class= "btn btn-dark" id="botonEliminar"><i class="fas fa-trash-alt"></i></button>
-                </div>    
-            
-            
-            </div>`
-        
-        contenedorCarrito.appendChild(div)
-
-        localStorage.setItem(`carrito`, JSON.stringify(carrito))
-
-        console.log(productoCarrito.cantidad)
-    })
+  
     contadorCarrito.innerText = carrito.length
     precioTotal.innerText = carrito.reduce((acum,productoCarrito)=> acum + productoCarrito.precio*productoCarrito.cantidad, 0)
 
     fcompra.addEventListener(`click`,()=>{
         carrito.length <= 0 ? true : false
-        carrito.length ? swal({
+        carrito.length ?
+        
+        swal({
             title: 'Compra realizada',
             icon: 'success',
             confirmButtonColor: 'green',
@@ -187,11 +197,7 @@ document.addEventListener(`DOMContentLoaded`,()=>{
             confirmButtonColor: 'green',
             timer:4500
         }) 
-    if(fcompra = true){
-    carrito.length = 0
-    localStorage.setItem(`carrito`,JSON.stringify(carrito))
-    actualizarCarrito()
-    }
+   
     })
 }
   
@@ -255,21 +261,21 @@ menorA.addEventListener(`click`,()=>{
 //Botones Carrito (el sumar y restar aun no encuentro la forma de hacerlo)
      const agregarUno = document.getElementById(`botonAgregar`)
 
-    // const sumarAlCarrito = (prodId) => {
-    //     const item = carrito.find((prod) => prod.id===prodId)
-    //     carrito.indexOf(item)
-    //     prodId.cantidad++
-    //    actualizarCarrito()
-    // }
+    const sumarAlCarrito = (prodId) => {
+        const item = carrito.find((prod) => prod.id===prodId)
+        carrito.indexOf(item)
+        prodId.cantidad++
+       actualizarCarrito()
+    }
     
     
-    //  const eliminarDelCarrito = (prodId) => {
-    //     const item = carrito.find((prod) => prod.id === prodId)
-    //     const indice = carrito.indexOf(item)
-    //     carrito.splice(indice,1)
-    //     localStorage.setItem(`carrito`,JSON.stringify(carrito))
-    //     actualizarCarrito()
-    //  }
+     const eliminarDelCarrito = (prodId) => {
+        const item = carrito.find((prod) => prod.id === prodId)
+        const indice = carrito.indexOf(item)
+        carrito.splice(indice,1)
+        localStorage.setItem(`carrito`,JSON.stringify(carrito))
+        actualizarCarrito()
+     }
     
      vaciarCarrito.addEventListener(`click`, () => {
         carrito.length = 0
@@ -277,8 +283,6 @@ menorA.addEventListener(`click`,()=>{
         actualizarCarrito()
     })
 
-    agregarUno.addEventListener(`click`,(productoCarrito) => {
-        productoCarrito.cantidad++
-    })
+   
 
     
